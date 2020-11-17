3dcd26de854df0c3a1d69b0410b00bf3bf891fa6
HoangViet <thvroyal@gmail.com>
2020-11-17 20:02:28 +0700

    set up store redux-thunk

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+
+import {store} from './store'
 
 import './assets/scss/sb-admin-2.scss'
 import './assets/fonts/awesome/css/awesomefont.min.css'
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store = {store} >
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
