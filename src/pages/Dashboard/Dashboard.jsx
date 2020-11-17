3f91195e7996845d3a93757fa339a1cfdbd9ba6e
HoangViet <thvroyal@gmail.com>
2020-11-17 20:28:06 +0700

    build structure folder

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Sidebar from "../../layouts/Sidebar/Sidebar";
+
+function Dashboard(props) {
+    return (
+        <Sidebar/>
+    )
+}
+
+export default Dashboard;
