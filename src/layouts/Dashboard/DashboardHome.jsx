bf6f8f40baff6e866d9069b6f43421c92a368226
HoangViet <thvroyal@gmail.com>
2020-11-25 23:51:09 +0700

    DashboardHome.jsx & active link Sidebar.jsx

--- src/layouts/Dashboard/DashboardHome.jsx
+++ src/layouts/Dashboard/DashboardHome.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import {withRouter} from "react-router";
+import CarBannerImg from "../../assets/img/car_banner.svg"
 
 function DashboardHome(props) {
     return (
-        <h1>This is Dashboard Home</h1>
+        <>
+            <div className="container-fluid">
+                <div className="card mb-3">
+                    <div className="card-body text-center p-5">
+                        <img src={CarBannerImg} alt="" style={{
+                            width: "50%",
+                        }}/>
+                        <h5 className="font-weight-bold text-primary">Dashboard Page Admin</h5>
+                        <p>You can create, edit, delete anything here.</p>
+                        <button className="btn btn-primary btn-lg" type="button">Get Start</button>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
