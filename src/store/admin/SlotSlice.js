a52fedfd7d30913169db944d404a8bf4a609cd43
Hoang Viet <thvroyal@gmail.com>
2021-04-26 13:22:42 +0700

    sortBydate

--- src/store/admin/SlotSlice.js
+++ src/store/admin/SlotSlice.js
@@ -1,29 +1,35 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import { sortBy } from "../../helpers/sort";
 
 const initState = {
-    loading: false,
-    data: null,
-    err: null,
-}
+  loading: false,
+  data: null,
+  err: null,
+};
 
 const slot = createSlice({
-    name: 'slot',
-    initialState: initState,
-    reducers: {
-        loadingSlot: (state, action) => {
-            state.loading = action.payload || true;
-        },
-        successSlot: (state, action) => {
-            state.data = action.payload;
-            state.loading = false;
-        },
-        failedSlot: (state, action) => {
-            state.err = action.payload;
-            state.loading = false;
-        }
-    }
+  name: "slot",
+  initialState: initState,
+  reducers: {
+    loadingSlot: (state, action) => {
+      state.loading = action.payload || true;
+    },
+    successSlot: (state, action) => {
+      state.data = action.payload;
+      state.loading = false;
+    },
+    failedSlot: (state, action) => {
+      state.err = action.payload;
+      state.loading = false;
+    },
+    sortSlot: (state, action) => {
+      let sortedState = [...state.data];
+      sortBy(sortedState, action.payload);
+      state.data = sortedState;
+    },
+  },
 });
 
-const {reducer, actions} = slot;
-export const {loadingSlot, successSlot, failedSlot} = actions;
+const { reducer, actions } = slot;
+export const { loadingSlot, successSlot, failedSlot, sortSlot } = actions;
 export default reducer;
