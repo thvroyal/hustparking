06c01132c72ba7b65b2f0557c926dfc5f7277aea
Hoang Viet <thvroyal@gmail.com>
2021-10-30 17:55:02 +0700

    analysis

---
+++ src/layouts/Dashboard/Analysis/mockData.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    time: 1622998800000,
+    totalSlot: 50,
+    freq: 1,
+    cost: 1200000,
+  },
+  {
+    time: 1622998800000,
+    totalSlot: 50,
+    freq: 1,
+    cost: 1200000,
+  },
+  {
+    time: 1622998800000,
+    totalSlot: 50,
+    freq: 1,
+    cost: 1200000,
+  },
+  {
+    time: 1622998800000,
+    totalSlot: 50,
+    freq: 1,
+    cost: 1200000,
+  },
+  {
+    time: 1622998800000,
+    totalSlot: 50,
+    freq: 1,
+    cost: 1200000,
+  },
+];
