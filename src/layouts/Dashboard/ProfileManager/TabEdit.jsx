e46dee97cccafb2b07ed2d33684911eddd480412
Hong Quang <quang.nh231@gmail.com>
2021-11-09 16:22:52 +0700

    edit profile manager

--- src/layouts/Dashboard/ProfileManager/TabEdit.jsx
+++ src/layouts/Dashboard/ProfileManager/TabEdit.jsx
@@ -60,7 +60,6 @@ function TabEdit() {
                 address: info.address,
                 birth: formatString(info.birth),
                 sex: info.sex,
-                equipment: info.equipment,
                 idNumber: info.idNumber,
               }}
               validationSchema={validate}
@@ -98,7 +97,6 @@ function TabEdit() {
                       <RadioField label="Other" name="sex" value="O" checked={values.sex === 'O'} />
                     </div>
                     <TextField showLabel label="address" name="address" type="text" placeholder="Update your address" />
-                    <TextField showLabel label="equipment" name="equipment" type="text" placeholder="Update your equipment" />
                     <TextField showLabel label="ID number" name="idNumber" type="number" placeholder="Update your ID number" />
                     <button
                       type="submit"
