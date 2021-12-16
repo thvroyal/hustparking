003f6f9b6ba45d0d4dcedc0c1864eec2b61ec9ee
Hoang Viet <thvroyal@gmail.com>
2021-12-16 19:59:03 +0700

    update

--- src/pages/VideoCaptureParking/index.jsx
+++ src/pages/VideoCaptureParking/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+const URL_LIVE = 'http://192.168.2.12:8000/stream';
+
 export default function VideoCaptureParking() {
   return (
     <div>
-      Hello
+      <img
+        src={URL_LIVE}
+        alt="video"
+        style={{
+          width: '100%',
+        }}
+      />
     </div>
   );
 }
