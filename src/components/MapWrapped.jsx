042c969c71c32cd6b2e3322d7cc407335601e567
Hong Quang <quang.nh231@gmail.com>
2022-04-07 12:58:07 +0700

    update live maps

--- src/components/MapWrapped.jsx
+++ src/components/MapWrapped.jsx
@@ -1,4 +1,4 @@
-// import {  } from '@react-google-maps/api';
+import { Autocomplete } from '@react-google-maps/api';
 import { string } from 'prop-types';
 import React, { useState } from 'react';
 import {
@@ -23,51 +23,78 @@ function Map({ listFields }) {
 
   // const options = { closeBoxURL: '', enableEventPropagation: true, visible: false };
   return (
-    <GoogleMap
-      defaultZoom={18}
-      defaultCenter={{ lat: latPosition, lng: longPosition }}
-      defaultOptions={{ styles: mapStyle }}
-    >
-      {listFields ? (
-        listFields.listOfFields.map((item) => (
-          <>
-            <Marker
-              key={item.id}
-              onClick={() => focusMarker(parseFloat(item.latitude), parseFloat(item.longitude))}
-              icon={{
-                scaledSize: new window.google.maps.Size(80, 80),
-              }}
-              position={{ lat: parseFloat(item.latitude), lng: parseFloat(item.longitude) }}
-            >
-              <InfoWindow
-                onCloseClick
+    <>
+      <div
+        className="d-flex justify-content-around align-items-center bg-white"
+        style={{
+          width: '573px',
+          height: '69px',
+          position: 'fixed',
+          top: '7%',
+          left: '30%',
+        }}
+      >
+        <div>
+          <Autocomplete>
+            <input type="text" placeholder="Origin" />
+          </Autocomplete>
+        </div>
+        <div>
+          <Autocomplete>
+            <input type="text" placeholder="Destination" />
+          </Autocomplete>
+        </div>
+        <button type="button" className="btn btn-primary">
+          Cal router
+        </button>
+      </div>
+
+      <GoogleMap
+        defaultZoom={18}
+        defaultCenter={{ lat: latPosition, lng: longPosition }}
+        defaultOptions={{ styles: mapStyle }}
+      >
+        {listFields ? (
+          listFields.listOfFields.map((item) => (
+            <>
+              <Marker
+                key={item.id}
+                onClick={() => focusMarker(parseFloat(item.latitude), parseFloat(item.longitude))}
+                icon={{
+                  scaledSize: new window.google.maps.Size(80, 80),
+                }}
+                position={{ lat: parseFloat(item.latitude), lng: parseFloat(item.longitude) }}
               >
-                <div>
-                  <table className="table">
-                    <thead>
-                      <tr>
-                        <th scope="row">#</th>
-                        <th scope="row">First</th>
-                        <th scope="row">Last</th>
-                        <th scope="row">Handle</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <th scope="col">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </InfoWindow>
-            </Marker>
-          </>
-        ))
-      ) : ''}
-    </GoogleMap>
+                <InfoWindow
+                  onCloseClick
+                >
+                  <div>
+                    <table className="table">
+                      <thead>
+                        <tr>
+                          <th scope="row">#</th>
+                          <th scope="row">First</th>
+                          <th scope="row">Last</th>
+                          <th scope="row">Handle</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th scope="col">1</th>
+                          <td>Mark</td>
+                          <td>Otto</td>
+                          <td>@mdo</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </InfoWindow>
+              </Marker>
+            </>
+          ))
+        ) : ''}
+      </GoogleMap>
+    </>
   );
 }
 
